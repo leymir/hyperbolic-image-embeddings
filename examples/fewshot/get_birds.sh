f5d0b85a59a85dbc19ff86ae7bd8f9ac2907d0e4
KhrulkovV <khrulkov.v@gmail.com>
2019-04-02 17:52:52 +0300

    fix tar

--- examples/fewshot/get_birds.sh
+++ examples/fewshot/get_birds.sh
@@ -1,6 +1,6 @@
 cd data/cub
 wget http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz
-unzip images.tgz
+tar -xzvf images.tgz
 rm images.tgz
 cd ../..
 mv data/cub/images/*/*.jpg data/cub/images
