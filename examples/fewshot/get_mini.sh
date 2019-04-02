76cdc15e378468643d7266e055fa0f55a0baf2ac
Valentin Khrulkov <khrulkov.v@gmail.com>
2019-04-02 13:08:15 +0000

    get mini

---
+++ examples/fewshot/get_mini.sh
@@ -0,0 +1,11 @@
+function gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+cd data/miniimagenet
+gdrive_download 1HkgrkAwukzEZA0TpO7010PkAOREb2Nuk images.zip
+unzip images.zip
+rm images.zip
+cd ../..
+
