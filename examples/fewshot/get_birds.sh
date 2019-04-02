849542d48bba9b27b93d90a833a5f492e328cf89
KhrulkovV <khrulkov.v@gmail.com>
2019-04-02 19:17:23 +0300

    fix birbs script

--- examples/fewshot/get_birds.sh
+++ examples/fewshot/get_birds.sh
@@ -1,6 +1,10 @@
+#!/usr/bin/env bash
 cd data/cub
-wget http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz
-tar -xzvf images.tgz
-rm images.tgz
+wget http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz
+tar -xzvf CUB_200_2011.tgz
+rm CUB_200_2011.tgz
+rm attributes.txt
+mkdir images
+mv CUB_200_2011/images/*/*.jpg images
+rm -r CUB_200_2011
 cd ../..
-mv data/cub/images/*/*.jpg data/cub/images
