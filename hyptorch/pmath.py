0a7bd9715a80f6818f4f6d4602f08ab779eb75d4
KhrulkovV <khrulkov.v@gmail.com>
2019-04-15 13:36:48 +0300

    fixed poincare mean

--- hyptorch/pmath.py
+++ hyptorch/pmath.py
@@ -421,9 +421,31 @@ def k2p(x, c):
     return x / denom
 
 
+def lorenz_factor(x, *, c=1.0, dim=-1, keepdim=False):
+    """
+
+    Parameters
+    ----------
+    x : tensor
+        point on Klein disk
+    c : float
+        negative curvature
+    dim : int
+        dimension to calculate Lorenz factor
+    keepdim : bool
+        retain the last dim? (default: false)
+
+    Returns
+    -------
+    tensor
+        Lorenz factor
+    """
+    return 1 / torch.sqrt(1 - c * x.pow(2).sum(dim=dim, keepdim=keepdim))
+
+
 def poincare_mean(x, dim=0, c=1.0):
     x = p2k(x, c)
-    lamb = lambda_x(x, c=c, keepdim=True)
+    lamb = lorenz_factor(x, c=c, keepdim=True)
     mean = torch.sum(lamb * x, dim=dim, keepdim=True) / torch.sum(lamb, dim=dim, keepdim=True)
     mean = k2p(mean, c)
     return mean.squeeze(dim)
